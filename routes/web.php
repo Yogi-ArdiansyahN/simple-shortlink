bcf14f5cd251c2e56da8d4b6ab1f5afe59fc1b87
abyanfalah <abyanf410@gmail.com>
2023-06-06 20:02:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [LinkController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::prefix('link')->group(function () {
+        Route::get('/', [LinkController::class, 'index']);
+        Route::get('create', [LinkController::class, 'create']);
+        Route::get('edit', [LinkController::class, 'edit']);
+
+        Route::post('/', [LinkController::class, 'store']);
+        Route::put('/{link}', [LinkController::class, 'update']);
+        Route::delete('/{link}', [LinkController::class, 'delete']);
+    });
+
+    Route::post('/logout', [LoginController::class, 'logout']);
+});
+
+
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authLogin']);
+
+    Route::get('/daftar', [AuthController::class, 'register']);
+    Route::post('/daftar', [AuthController::class, 'authRegister']);
+});
+
+// redirect user/guest ke link original via shortlink
+Route::get('/(:any)', [LinkController::class, 'redirect']);
+
+Route::get('/test', function () {
+    // return App::make('url')->to('/');
+    return auth()->user();
 });
